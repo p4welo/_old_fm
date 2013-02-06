259b7a12ece4ac210bfee13b2d495a0dc14d7873
p4welo@gmail.com <p4welo@gmail.com@82e6a113-ac8b-97a3-c4c2-d225cff0968c>
2013-02-06 16:54:33 +0000

    Bootstrap

--- trunk/admin/src/main/java/com/football/manager/admin/pages/LeagueListPage.java
+++ trunk/admin/src/main/java/com/football/manager/admin/pages/LeagueListPage.java
@@ -3,12 +3,11 @@ package com.football.manager.admin.pages;
 import com.football.manager.admin.api.AdminApiMappings;
 import com.football.manager.admin.pages.cmp.table.AjaxDataTable;
 import com.football.manager.admin.pages.cmp.table.DataProvider;
-import com.football.manager.admin.pages.cmp.window.CreateNewLeagueWindow;
+import com.football.manager.admin.pages.cmp.window.CreateNewLeagueModal;
 import com.football.manager.domain.DataEntity;
 import com.football.manager.domain.League;
 import com.football.manager.service.ILeagueService;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -34,7 +33,7 @@ public class LeagueListPage extends AbstractPage
 
    private final WebMarkupContainer mainContainer;
 
-   private CreateNewLeagueWindow createNewLeagueWindow;
+   private CreateNewLeagueModal createNewLeagueWindow;
 
    public LeagueListPage()
    {
@@ -56,30 +55,32 @@ public class LeagueListPage extends AbstractPage
 
    private void createTableToolbar()
    {
-      mainContainer.add(new AjaxLink<Void>("newLeagueLink")
-      {
-         @Override
-         public void onClick(AjaxRequestTarget ajaxRequestTarget)
-         {
-            createNewLeagueWindow.show(ajaxRequestTarget);
-         }
-      });
+//      mainContainer.add(new AjaxLink<Void>("newLeagueLink")
+//      {
+//         @Override
+//         public void onClick(AjaxRequestTarget ajaxRequestTarget)
+//         {
+//            createNewLeagueWindow.show(ajaxRequestTarget);
+//         }
+//      });
    }
 
    private void createNewLeagueWindow()
    {
-      createNewLeagueWindow = new CreateNewLeagueWindow("createNewLeagueWindow")
-      {
-         @Override
-         protected void onConfirm(AjaxRequestTarget target)
-         {
-            League newLeague = new League();
-            newLeague.setName(createNewLeagueWindow.getLeagueName());
-            leagueService.save(newLeague, createNewLeagueWindow.getGenerateTeams());
-            target.add(mainContainer);
-         }
-      };
-      add(createNewLeagueWindow);
+      createNewLeagueWindow = new CreateNewLeagueModal("modal");
+      add(new CreateNewLeagueModal("modal"));
+//      createNewLeagueWindow = new CreateNewLeagueWindow("createNewLeagueWindow")
+//      {
+//         @Override
+//         protected void onConfirm(AjaxRequestTarget target)
+//         {
+//            League newLeague = new League();
+//            newLeague.setName(createNewLeagueWindow.getLeagueName());
+//            leagueService.save(newLeague, createNewLeagueWindow.getGenerateTeams());
+//            target.add(mainContainer);
+//         }
+//      };
+//      add(createNewLeagueWindow);
    }
 
    private void createLeagueTable()
---
+++ trunk/admin/src/main/java/com/football/manager/admin/pages/cmp/window/CreateNewLeagueModal.java
@@ -0,0 +1,21 @@
+package com.football.manager.admin.pages.cmp.window;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+
+/**
+ * User: pawel.radomski
+ * Date: 06.02.13
+ * Time: 17:41
+ */
+public class CreateNewLeagueModal extends AbstractModal
+{
+   public CreateNewLeagueModal(String id)
+   {
+      super(id);
+   }
+
+   @Override
+   protected void onConfirm(AjaxRequestTarget target)
+   {
+   }
+}
