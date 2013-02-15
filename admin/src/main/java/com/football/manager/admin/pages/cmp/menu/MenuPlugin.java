4b366e1d22b0e0645e3d31caa8ff4d3cdeaa24b1
p4welo <p4welo@gmail.com>
2013-02-15 15:56:45 +0000

    menu fix

--- admin/src/main/java/com/football/manager/admin/pages/cmp/menu/MenuPlugin.java
+++ admin/src/main/java/com/football/manager/admin/pages/cmp/menu/MenuPlugin.java
@@ -2,14 +2,15 @@ package com.football.manager.admin.pages.cmp.menu;
 
 import com.football.manager.admin.AdminApplication;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.MarkupContainer;
-import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 
 import java.util.ArrayList;
@@ -24,7 +25,7 @@ public class MenuPlugin extends Panel
 {
    private List<MenuItem> menuItemList = new ArrayList<MenuItem>();
 
-   private static String selectedItem = "";
+   private static String selectedItem;
 
    public MenuPlugin(String id)
    {
@@ -43,6 +44,19 @@ public class MenuPlugin extends Panel
       {
          private static final long serialVersionUID = -7597884223556472130L;
 
+         @Override
+         protected void onBeforeRender()
+         {
+            for (MenuItem menuItem : MenuPlugin.this.menuItemList)
+            {
+               if (getPage().getClass() == menuItem.getTarget())
+               {
+                  selectedItem = menuItem.getTarget().toString();
+               }
+            }
+            super.onBeforeRender();
+         }
+
          protected void populateItem(ListItem item)
          {
             final MenuItem menuItem = (MenuItem) item.getModelObject();
@@ -52,20 +66,14 @@ public class MenuPlugin extends Panel
                @Override
                public void onClick()
                {
-                  selectedItem = menuItem.getTarget().toString();
                   setResponsePage(menuItem.getTarget());
                }
-
-               @Override
-               protected void onComponentTag(ComponentTag tag)
-               {
-                  if (menuItem.getTarget().toString().equals(selectedItem))
-                  {
-                     tag.put("class", "active");
-                  }
-                  super.onComponentTag(tag);
-               }
             };
+
+            if (menuItem.getTarget().toString().equals(selectedItem))
+            {
+               item.add(new AttributeModifier("class", new Model("active")));
+            }
 
             Label label = new Label("label", new ResourceModel(menuItem.getResourceKey()));
             MarkupContainer container = link.add(label);
