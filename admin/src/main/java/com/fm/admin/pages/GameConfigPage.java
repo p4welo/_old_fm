ac78c85e052d6a0019e35c016a1cd2fa269396f1
p4welo <p4welo@gmail.com>
2013-03-28 23:00:37 +0000

    small changes

--- admin/src/main/java/com/fm/admin/pages/GameConfigPage.java
+++ admin/src/main/java/com/fm/admin/pages/GameConfigPage.java
@@ -1,7 +1,11 @@
 package com.fm.admin.pages;
 
 import com.fm.admin.api.AdminApiMappings;
+import com.fm.service.IPositionService;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
 
 /**
@@ -15,9 +19,26 @@ import org.wicketstuff.annotation.mount.MountPath;
 @AuthorizeInstantiation("ROLE_ADMIN")
 public class GameConfigPage extends AdminAbstractPage
 {
+   @SpringBean
+   private IPositionService positionService;
+
+   private long positionCount;
+
    public GameConfigPage()
    {
       super();
+      positionCount = positionService.getCount();
+      add(new Label("positionsCount", new PropertyModel(this, "positionCount")));
+   }
+
+   public long getPositionCount()
+   {
+      return positionCount;
+   }
+
+   public void setPositionCount(long positionCount)
+   {
+      this.positionCount = positionCount;
    }
 
    @Override
