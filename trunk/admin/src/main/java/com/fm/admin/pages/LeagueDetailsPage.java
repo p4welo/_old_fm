d047e8cde1ae9ee0517a62f52005bbf8608c309d
p4welo@gmail.com <p4welo@gmail.com@82e6a113-ac8b-97a3-c4c2-d225cff0968c>
2013-04-11 21:54:42 +0000

    abstract window

--- trunk/admin/src/main/java/com/fm/admin/pages/LeagueDetailsPage.java
+++ trunk/admin/src/main/java/com/fm/admin/pages/LeagueDetailsPage.java
@@ -4,13 +4,19 @@ import com.fm.admin.api.AdminApiMappings;
 import com.fm.admin.cmp.breadcrumb.LeagueDetailsBreadcrumb;
 import com.fm.admin.cmp.leagueDetails.LeagueInfoPanel;
 import com.fm.admin.cmp.leagueDetails.LeagueTeamsPanel;
+import com.fm.admin.cmp.window.NewTeamWindow;
 import com.fm.core.cmp.breadcrumb.BootstrapBreadcrumbPanel;
 import com.fm.domain.League;
+import com.fm.domain.Team;
 import com.fm.service.ILeagueService;
+import com.fm.service.ITeamService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.RestartResponseAtInterceptPageException;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
@@ -27,6 +33,11 @@ public class LeagueDetailsPage extends AdminAbstractPage
 {
    @SpringBean
    private ILeagueService leagueService;
+
+   @SpringBean
+   private ITeamService teamService;
+
+   private NewTeamWindow window;
 
    private League league;
 
@@ -63,8 +74,31 @@ public class LeagueDetailsPage extends AdminAbstractPage
 
    private void initView()
    {
+      window = new NewTeamWindow("window", getString("create.team"), league)
+      {
+         @Override
+         public void onSubmit(AjaxRequestTarget target, Form<?> form)
+         {
+            Team team = getTeam();
+            teamService.save(team);
+
+            success(getString("team.successfully.saved"));
+
+            resetState();
+            target.add(mainContainer);
+         }
+      };
+      add(window);
       mainContainer.add(new LeagueInfoPanel("info", league));
       mainContainer.add(new LeagueTeamsPanel("teams", league));
+      mainContainer.add(new AjaxLink<Void>("newTeam")
+      {
+         @Override
+         public void onClick(AjaxRequestTarget target)
+         {
+            window.show(target);
+         }
+      });
       add(mainContainer);
    }
 
