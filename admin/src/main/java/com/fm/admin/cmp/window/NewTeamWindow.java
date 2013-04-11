3f72ae586480ba74a627907c5817e91b1d4fd5d4
p4welo <p4welo@gmail.com>
2013-04-11 21:54:42 +0000

    abstract window

--- admin/src/main/java/com/fm/admin/cmp/window/NewTeamWindow.java
+++ admin/src/main/java/com/fm/admin/cmp/window/NewTeamWindow.java
@@ -4,31 +4,30 @@ import com.fm.core.cmp.web.BootstrapCheckBoxPanel;
 import com.fm.core.cmp.web.BootstrapTextFieldPanel;
 import com.fm.core.cmp.window.AbstractWindow;
 import com.fm.domain.League;
-import com.fm.service.ILeagueService;
+import com.fm.domain.Team;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
  * Created with IntelliJ IDEA.
  * User: Barbara
- * Date: 06.04.13
- * Time: 10:57
+ * Date: 11.04.13
+ * Time: 23:42
  * To change this template use File | Settings | File Templates.
  */
-public class NewLeagueWindow extends AbstractWindow
+public class NewTeamWindow extends AbstractWindow
 {
-   @SpringBean
-   private ILeagueService leagueService;
-
    private League league;
 
-   private Boolean generateTeams;
+   private Team team;
 
-   public NewLeagueWindow(String id, String header)
+   private Boolean generatePlayers;
+
+   public NewTeamWindow(String id, String header, League league)
    {
       super(id, header);
+      this.league = league;
       resetState();
       initView();
    }
@@ -36,18 +35,23 @@ public class NewLeagueWindow extends AbstractWindow
    private void initView()
    {
       BootstrapTextFieldPanel name = new BootstrapTextFieldPanel("name",
-              new PropertyModel(this, "league." + League.FIELD_NAME), "span9");
+              new PropertyModel(this, "team." + Team.FIELD_NAME), "span9");
       name.setValidation();
       form.add(name);
-      BootstrapCheckBoxPanel checkBox = new BootstrapCheckBoxPanel("generateTeams",
-              new PropertyModel<Boolean>(this, "generateTeams"));
+      BootstrapTextFieldPanel account = new BootstrapTextFieldPanel("account",
+              new PropertyModel(this, "team." + Team.FIELD_ACCOUNT), "span9");
+      account.setValidation();
+      form.add(account);
+      BootstrapCheckBoxPanel checkBox = new BootstrapCheckBoxPanel("generatePlayers",
+              new PropertyModel<Boolean>(this, "generatePlayers"));
       form.add(checkBox);
    }
 
    public void resetState()
    {
-      league = new League();
-      generateTeams = Boolean.FALSE;
+      team = new Team();
+      team.setLeague(league);
+      generatePlayers = Boolean.FALSE;
    }
 
    @Override
@@ -60,13 +64,13 @@ public class NewLeagueWindow extends AbstractWindow
    {
    }
 
-   public League getLeague()
+   public Team getTeam()
    {
-      return league;
+      return team;
    }
 
-   public Boolean getGenerateTeams()
+   public Boolean getGeneratePlayers()
    {
-      return generateTeams;
+      return generatePlayers;
    }
 }
