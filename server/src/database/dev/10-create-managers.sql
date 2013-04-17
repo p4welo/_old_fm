24ce090c780d63a620693a8ff8005ddda6b4c275
p4welo <p4welo@gmail.com>
2013-04-17 06:36:14 +0000

    OSD Integration

--- server/src/database/dev/10-create-managers.sql
+++ server/src/database/dev/10-create-managers.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-INSERT INTO manager (sid, name, surname, team_id, user_id) VALUES ('3f7e06822c0541e495078aed80afkdlf', 'Włodzimierz', 'Górski', null, (SELECT id FROM user WHERE login='user'));
+INSERT INTO manager (sid, name, surname, team_id, user_id) VALUES ('3f7e06822c0541e495078aed80afkdlf', 'Jacek', 'Macaj', null, (SELECT id FROM user WHERE login='user'));
 
 COMMIT;
