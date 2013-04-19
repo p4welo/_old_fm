5dc036ba7a69d0086e2aac00872537d475f9f44d
p4welo <p4welo@gmail.com>
2013-04-19 09:00:23 +0000

    filter toolbar

--- server/src/database/dev/10-create-managers.sql
+++ server/src/database/dev/10-create-managers.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-INSERT INTO manager (sid, name, surname, team_id, user_id) VALUES ('3f7e06822c0541e495078aed80afkdlf', 'Jacek', 'Macaj', null, (SELECT id FROM user WHERE login='user'));
+INSERT INTO manager (sid, name, surname, team_id, user_id) VALUES ('3f7e06822c0541e495078aed80afkdlf', 'Jacek', 'Macaj', (SELECT id FROM team WHERE sid='8kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM user WHERE login='user'));
 
 COMMIT;
