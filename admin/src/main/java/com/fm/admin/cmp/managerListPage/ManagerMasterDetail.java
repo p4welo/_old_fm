2c9feea29fa0650bdc4d9c63849fce6fb6f2956a
p4welo <p4welo@gmail.com>
2013-04-23 15:25:21 +0000

    user objectstate

---
+++ admin/src/main/java/com/fm/admin/cmp/managerListPage/ManagerMasterDetail.java
@@ -0,0 +1,83 @@
+package com.fm.admin.cmp.managerListPage;
+
+import com.fm.admin.cmp.managerListPage.table.ManagerFilterToolbar;
+import com.fm.core.cmp.masterDetail.MasterDetailPanel;
+import com.fm.core.cmp.newTable.AjaxDataTable;
+import com.fm.core.cmp.newTable.DataProvider;
+import com.fm.core.cmp.table.column.TranslatedColumn;
+import com.fm.domain.Manager;
+import com.fm.domain.ObjectStateEnum;
+import com.fm.domain.User;
+import com.fm.domain.filter.FmFilter;
+import com.fm.domain.filter.OpenSearchDescription;
+import com.fm.service.IManagerService;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: pawel.radomski
+ * Date: 23.04.13
+ * Time: 17:12
+ */
+public class ManagerMasterDetail extends MasterDetailPanel<Manager>
+{
+   @SpringBean
+   private IManagerService managerService;
+
+   public ManagerMasterDetail(String id)
+   {
+      super(id);
+   }
+
+   @Override
+   protected Panel provideDetailsPanel(String id, IModel<Manager> model)
+   {
+      return new ManagerDetailsPanel(id, model);
+   }
+
+   @Override
+   protected AjaxDataTable<Manager> provideMasterTable(String id)
+   {
+      List<IColumn<Manager, String>> columns = new ArrayList<IColumn<Manager, String>>();
+
+      columns.add(new PropertyColumn<Manager, String>(
+              new ResourceModel("manager.name"),
+              Manager.FIELD_NAME,
+              Manager.FIELD_NAME
+      ));
+      columns.add(new PropertyColumn<Manager, String>(
+              new ResourceModel("manager.surname"),
+              Manager.FIELD_SURNAME,
+              Manager.FIELD_SURNAME
+      ));
+      columns.add(new PropertyColumn<Manager, String>(
+              new ResourceModel("user.login"),
+              Manager.FIELD_USER + "." + User.FIELD_LOGIN
+      ));
+      columns.add(new PropertyColumn<Manager, String>(
+              new ResourceModel("user.email"),
+              Manager.FIELD_USER + "." + User.FIELD_EMAIL
+      ));
+      columns.add(new TranslatedColumn<Manager, String>(
+              new ResourceModel("object.state"),
+              null,
+              Manager.FIELD_USER + "." + User.FIELD_OBJECT_STATE,
+              ObjectStateEnum.class.getSimpleName()
+      ));
+      OpenSearchDescription<Manager> osd = new OpenSearchDescription<Manager>();
+      FmFilter filter = new FmFilter();
+      osd.setFilter(filter);
+      DataProvider dataProvider = new DataProvider(managerService, osd);
+
+      AjaxDataTable<Manager> table = new AjaxDataTable<Manager>(id, columns, dataProvider);
+      table.addFilterToolbar(new ManagerFilterToolbar(table));
+      return table;
+   }
+}
--- admin/src/main/java/com/fm/admin/pages/ManagerListPage.java
+++ admin/src/main/java/com/fm/admin/pages/ManagerListPage.java
@@ -2,33 +2,15 @@ package com.fm.admin.pages;
 
 import com.fm.admin.api.AdminApiMappings;
 import com.fm.admin.cmp.breadcrumb.ManagerListBreadcrumb;
-import com.fm.admin.cmp.managerListPage.ManagerDetailsPanel;
-import com.fm.admin.cmp.managerListPage.table.ManagerFilterToolbar;
+import com.fm.admin.cmp.managerListPage.ManagerMasterDetail;
 import com.fm.core.cmp.authorization.UserAuthorities;
 import com.fm.core.cmp.breadcrumb.BootstrapBreadcrumbPanel;
-import com.fm.core.cmp.newTable.AjaxDataTable;
-import com.fm.core.cmp.newTable.DataProvider;
-import com.fm.core.cmp.newTable.SelectionChangeCallback;
-import com.fm.core.cmp.table.column.TranslatedColumn;
 import com.fm.domain.Manager;
-import com.fm.domain.ObjectStateEnum;
-import com.fm.domain.User;
-import com.fm.domain.filter.FmFilter;
-import com.fm.domain.filter.OpenSearchDescription;
 import com.fm.service.IManagerService;
-import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * User: pawel.radomski
@@ -49,64 +31,12 @@ public class ManagerListPage extends AdminAbstractPage
    public ManagerListPage()
    {
       super();
-      main = new WebMarkupContainer("main");
-      main.setOutputMarkupId(true);
-
       initView();
    }
 
    private void initView()
    {
-      createUserTable();
-      add(main);
-   }
-
-   private void createUserTable()
-   {
-      List<IColumn<Manager, String>> columns = new ArrayList<IColumn<Manager, String>>();
-
-      columns.add(new PropertyColumn<Manager, String>(
-              new ResourceModel("manager.name"),
-              Manager.FIELD_NAME,
-              Manager.FIELD_NAME
-      ));
-      columns.add(new PropertyColumn<Manager, String>(
-              new ResourceModel("manager.surname"),
-              Manager.FIELD_SURNAME,
-              Manager.FIELD_SURNAME
-      ));
-      columns.add(new PropertyColumn<Manager, String>(
-              new ResourceModel("user.login"),
-              Manager.FIELD_USER + "." + User.FIELD_LOGIN
-      ));
-      columns.add(new PropertyColumn<Manager, String>(
-              new ResourceModel("user.email"),
-              Manager.FIELD_USER + "." + User.FIELD_EMAIL
-      ));
-      columns.add(new TranslatedColumn<Manager, String>(
-              new ResourceModel("object.state"),
-              null,
-              Manager.FIELD_USER + "." + User.FIELD_OBJECT_STATE,
-              ObjectStateEnum.class.getSimpleName()
-      ));
-      OpenSearchDescription<Manager> osd = new OpenSearchDescription<Manager>();
-      FmFilter filter = new FmFilter();
-      osd.setFilter(filter);
-      DataProvider dataProvider = new DataProvider(managerService, osd);
-
-      AjaxDataTable<Manager> table = new AjaxDataTable<Manager>("table", columns, dataProvider);
-      table.setSelectionChangeCallback(new SelectionChangeCallback<Manager>()
-      {
-         @Override
-         public void onSelectionChange(AjaxRequestTarget target, IModel<Manager> model)
-         {
-            selected = model.getObject();
-            target.add(main);
-         }
-      });
-      main.add(table);
-      table.addFilterToolbar(new ManagerFilterToolbar(table));
-      main.add(new ManagerDetailsPanel("details", new PropertyModel<Manager>(this, "selected")));
+      add(new ManagerMasterDetail("masterDetail"));
    }
 
    @Override
