07bfe77be9b3f5655e79c383710b3a0ca27716da
p4welo <p4welo@gmail.com>
2013-04-24 15:34:49 +0000

    notification fix

--- server/src/database/dev/9-create-team-records.sql
+++ server/src/database/dev/9-create-team-records.sql
@@ -1,63 +1,63 @@
 BEGIN;
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvj22s22c0541e491078aed80a96s33', (SELECT id FROM team WHERE sid='kkvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='kkvjjc822c0541e495078aed80a96s33'), 4, 10, 6, 2, 4, 3, 1, 0);
+('kkvj22s22c0541e491078aed80a96s33', (SELECT id FROM team WHERE sid='kkvjjc822c05z1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='kkvjjc822c05z1e495078aed80a96s33'), 4, 10, 6, 2, 4, 3, 1, 0);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc82ldk541e491078aed80a96s33', (SELECT id FROM team WHERE sid='qkvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='qkvjjc822c0541e495078aed80a96s33'), 4, 4, 5, 3, 2, 1, 1, 2);
+('kkvjjc82ldk541e491078aed80a96s33', (SELECT id FROM team WHERE sid='qkvjjc822c05x1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='qkvjjc822c05x1e495078aed80a96s33'), 4, 4, 5, 3, 2, 1, 1, 2);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c0541oei1078aed80a96s33', (SELECT id FROM team WHERE sid='1kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='1kvjjc822c0541e495078aed80a96s33'), 4, 4, 7, 3, 4, 1, 1, 2);
+('kkvjjc822c0541oei1078aed80a96s33', (SELECT id FROM team WHERE sid='1kvjjc822c05c1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='1kvjjc822c05c1e495078aed80a96s33'), 4, 4, 7, 3, 4, 1, 1, 2);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c054lvk91078aed80a96s33', (SELECT id FROM team WHERE sid='2kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='2kvjjc822c0541e495078aed80a96s33'), 4, 4, 5, 10, -5, 1, 3, 0);
+('kkvjjc822c054lvk91078aed80a96s33', (SELECT id FROM team WHERE sid='2kvjjc822c05v1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='2kvjjc822c05v1e495078aed80a96s33'), 4, 4, 5, 10, -5, 1, 3, 0);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c054ppp91078aed80a96s33', (SELECT id FROM team WHERE sid='3kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='3kvjjc822c0541e495078aed80a96s33'), 4, 10, 1, 5, -4, 3, 1, 0);
+('kkvjjc822c054ppp91078aed80a96s33', (SELECT id FROM team WHERE sid='3kvjjc822c05b1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='3kvjjc822c05b1e495078aed80a96s33'), 4, 10, 1, 5, -4, 3, 1, 0);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc82lcl541e491078aed80a96s33', (SELECT id FROM team WHERE sid='4kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='4kvjjc822c0541e495078aed80a96s33'), 4, 1, 6, 5, 1, 0, 1, 3);
+('kkvjjc82lcl541e491078aed80a96s33', (SELECT id FROM team WHERE sid='4kvjjc822c05n1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='4kvjjc822c05n1e495078aed80a96s33'), 4, 1, 6, 5, 1, 0, 1, 3);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c0541e491sssaed80a96s33', (SELECT id FROM team WHERE sid='5kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='5kvjjc822c0541e495078aed80a96s33'), 4, 2, 5, 3, 2, 0, 2, 2);
+('kkvjjc822c0541e491sssaed80a96s33', (SELECT id FROM team WHERE sid='5kvjjc822c05m1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='5kvjjc822c05m1e495078aed80a96s33'), 4, 2, 5, 3, 2, 0, 2, 2);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c05eyr491078aed80a96s33', (SELECT id FROM team WHERE sid='6kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='6kvjjc822c0541e495078aed80a96s33'), 4, 4, 12, 9, 3, 1, 1, 2);
+('kkvjjc822c05eyr491078aed80a96s33', (SELECT id FROM team WHERE sid='6kvjjc822c05h1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='6kvjjc822c05h1e495078aed80a96s33'), 4, 4, 12, 9, 3, 1, 1, 2);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c0541ewwe078aed80a96s33', (SELECT id FROM team WHERE sid='7kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='7kvjjc822c0541e495078aed80a96s33'), 4, 4, 5, 7, -2, 1, 1, 2);
+('kkvjjc822c0541ewwe078aed80a96s33', (SELECT id FROM team WHERE sid='7kvjjc822c05g1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='7kvjjc822c05g1e495078aed80a96s33'), 4, 4, 5, 7, -2, 1, 1, 2);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c0541e491pclaed80a96s33', (SELECT id FROM team WHERE sid='8kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='8kvjjc822c0541e495078aed80a96s33'), 4, 8, 20, 5, 15, 2, 2, 0);
+('kkvjjc822c0541e491pclaed80a96s33', (SELECT id FROM team WHERE sid='8kvjjc822c05f1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='8kvjjc822c05f1e495078aed80a96s33'), 4, 8, 20, 5, 15, 2, 2, 0);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c0541e491qweaed80a96s33', (SELECT id FROM team WHERE sid='9kvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='9kvjjc822c0541e495078aed80a96s33'), 4, 10, 13, 11, 2, 3, 1, 0);
+('kkvjjc822c0541e491qweaed80a96s33', (SELECT id FROM team WHERE sid='9kvjjc822c05d1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='9kvjjc822c05d1e495078aed80a96s33'), 4, 10, 13, 11, 2, 3, 1, 0);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c05xcc491078aed80a96s33', (SELECT id FROM team WHERE sid='fkvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='fkvjjc822c0541e495078aed80a96s33'), 4, 12, 23, 1, 22, 4, 0, 0);
+('kkvjjc822c05xcc491078aed80a96s33', (SELECT id FROM team WHERE sid='fkvjjc822c05s1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='fkvjjc822c05s1e495078aed80a96s33'), 4, 12, 23, 1, 22, 4, 0, 0);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c05mcn491078aed80a96s33', (SELECT id FROM team WHERE sid='hkvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='hkvjjc822c0541e495078aed80a96s33'), 4, 4, 5, 11, -6, 1, 1, 2);
+('kkvjjc822c05mcn491078aed80a96s33', (SELECT id FROM team WHERE sid='hkvjjc822c05a1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='hkvjjc822c05a1e495078aed80a96s33'), 4, 4, 5, 11, -6, 1, 1, 2);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822iie41e491078aed80a96s33', (SELECT id FROM team WHERE sid='jkvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='jkvjjc822c0541e495078aed80a96s33'), 4, 0, 3, 0, 3, 0, 0, 4);
+('kkvjjc822iie41e491078aed80a96s33', (SELECT id FROM team WHERE sid='jkvjjc822c05q1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='jkvjjc822c05q1e495078aed80a96s33'), 4, 0, 3, 0, 3, 0, 0, 4);
 
 INSERT INTO team_record (sid, team_id, season_id, team_name, round_number, points_count, goals_scored, goals_allowed, goals_difference, wins_count, draws_count, loses_count) VALUES
-('kkvjjc822c0541e491llfaed80a96s33', (SELECT id FROM team WHERE sid='ykvjjc822c0541e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
-(SELECT name FROM team WHERE sid='ykvjjc822c0541e495078aed80a96s33'), 4, 4, 3, 2, 1, 0, 4, 0);
+('kkvjjc822c0541e491llfaed80a96s33', (SELECT id FROM team WHERE sid='ykvjjc822c05w1e495078aed80a96s33'), (SELECT id FROM season WHERE sid='885jjc822c0541e495078aed80a96s33'),
+(SELECT name FROM team WHERE sid='ykvjjc822c05w1e495078aed80a96s33'), 4, 4, 3, 2, 1, 0, 4, 0);
 
 COMMIT;
