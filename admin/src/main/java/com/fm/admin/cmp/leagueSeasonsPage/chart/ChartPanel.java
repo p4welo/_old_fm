803b447cc6271a6ea0b49a3fb918395dc7ea62c6
p4welo <p4welo@gmail.com>
2013-04-26 09:36:59 +0000

    redrawing chart

--- admin/src/main/java/com/fm/admin/cmp/leagueSeasonsPage/chart/ChartPanel.java
+++ admin/src/main/java/com/fm/admin/cmp/leagueSeasonsPage/chart/ChartPanel.java
@@ -3,6 +3,8 @@ package com.fm.admin.cmp.leagueSeasonsPage.chart;
 import com.fm.domain.Season;
 import com.fm.domain.Team;
 import com.fm.domain.TeamRecord;
+import com.fm.domain.defined.SystemParameters;
+import com.fm.service.ISystemParameterService;
 import com.fm.service.ITeamRecordService;
 import com.googlecode.wickedcharts.highcharts.options.*;
 import com.googlecode.wickedcharts.highcharts.options.series.SimpleSeries;
@@ -12,7 +14,6 @@ import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -26,6 +27,9 @@ public class ChartPanel extends Panel
 {
    @SpringBean
    private ITeamRecordService teamRecordService;
+
+   @SpringBean
+   private ISystemParameterService systemParameterService;
 
    private List<TeamRecord> records;
 
@@ -70,47 +74,36 @@ public class ChartPanel extends Panel
          xCategoryList.add(String.valueOf(i + 1));
          series.add(records.get(i).getPlace());
       }
-      for (int i = 17; i > 0; i--)
+
+      int teamCount = Integer.valueOf(systemParameterService.getByKey(SystemParameters.TEAM_COUNT_PER_LEAGUE));
+      for (int i = teamCount; i > 0; i--)
       {
          yCategoryList.add(String.valueOf(i));
       }
       options.setxAxis(new Axis().setCategories(xCategoryList));
-      options.setyAxis(new Axis().setMax(16).setMin(1).setTitle(new Title("Miejsce")));
+      options.setyAxis(new Axis().setReversed(true).setMax(16).setMin(1).setAllowDecimals(false)
+              .setTitle(new Title(getString("position"))));
+      options.setTitle(new Title(getString("chart.title")));
       options.setLegend(new Legend().setLayout(LegendLayout.VERTICAL)
               .setAlign(HorizontalAlignment.CENTER)
               .setVerticalAlign(VerticalAlignment.BOTTOM)
               .setX(-10)
               .setY(100)
               .setBorderWidth(0));
-      options.addSeries(new SimpleSeries().setName("Tokyo").setData(series));
+      options.addSeries(new SimpleSeries().setName(getString("seriesName")).setData(series));
       return options;
    }
 
    private void initView()
    {
-      Options options = new Options();
-
-      options.setChartOptions(new ChartOptions()
-              .setType(SeriesType.COLUMN));
-
-      options.setTitle(new Title("My very own chart."));
-
-      options.setxAxis(new Axis().setCategories(Arrays.asList(new String[]{"1", "2", "3", "4", "5",
-              "6", "7", "8", "9", "10", "11", "12"})));
-
-      options.setyAxis(new Axis().setTitle(new Title("Miejsce")));
-
-      options.setLegend(new Legend().setLayout(LegendLayout.VERTICAL)
-              .setAlign(HorizontalAlignment.CENTER)
-              .setVerticalAlign(VerticalAlignment.BOTTOM)
-              .setX(-10)
-              .setY(100)
-              .setBorderWidth(0));
-
-      options.addSeries(new SimpleSeries().setName("Tokyo").setData(
-              Arrays.asList(new Number[]{7.0, 6.9, 9.5, 14.5, 18.2, 21.5,
-                      25.2, 26.5, 23.3, 18.3, 13.9, 9.6})));
-      chart = new Chart("chart", options);
+      chart = new Chart("chart", new Options().setChartOptions(new ChartOptions()))
+      {
+         @Override
+         protected void onConfigure()
+         {
+            setVisible(selectedModel.getObject() != null);
+         }
+      };
       add(chart);
    }
 }
