973f89a8504c7f357919d866633586fd1fbeba7c
p4welo@gmail.com <p4welo@gmail.com@82e6a113-ac8b-97a3-c4c2-d225cff0968c>
2013-05-05 13:50:21 +0000

    charts

---
+++ trunk/service/src/main/java/com/fm/service/IPlayerGenerationStrategy.java
@@ -0,0 +1,13 @@
+package com.fm.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Barbara
+ * Date: 05.05.13
+ * Time: 15:38
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IPlayerGenerationStrategy
+{
+   int getPotential(int age);
+}
