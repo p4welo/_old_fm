520f97a980cee94edbde11d7998cc2aefb493815
p4welo@gmail.com <p4welo@gmail.com@82e6a113-ac8b-97a3-c4c2-d225cff0968c>
2013-05-16 11:55:14 +0000

    SeasonCombo

--- trunk/admin/src/main/java/com/fm/admin/pages/AdminAbstractPage.java
+++ trunk/admin/src/main/java/com/fm/admin/pages/AdminAbstractPage.java
@@ -5,7 +5,9 @@ import com.fm.admin.cmp.menu.AdminMenuPanel;
 import com.fm.admin.pages.leagueListPage.LeagueListPage;
 import com.fm.core.cmp.authorization.LogoutLink;
 import com.fm.core.cmp.breadcrumb.BootstrapBreadcrumbPanel;
+import org.apache.wicket.devutils.debugbar.DebugBar;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.panel.EmptyPanel;
 
 import java.io.Serializable;
 
@@ -24,6 +26,14 @@ public abstract class AdminAbstractPage extends WebPage implements AdminApiMappi
       add(new AdminMenuPanel("menu"));
       add(new LogoutLink("logout", LeagueListPage.class));
       add(provideBreadcrumb("breadcrumb"));
+      if (getApplication().getDebugSettings().isDevelopmentUtilitiesEnabled())
+      {
+         add(new DebugBar("dev"));
+      }
+      else
+      {
+         add(new EmptyPanel("dev").setVisible(false));
+      }
    }
 
    protected abstract BootstrapBreadcrumbPanel provideBreadcrumb(String id);
