c2dcebac681fa393dfa36ebe780efcea7638f153
p4welo <p4welo@gmail.com>
2013-05-16 16:01:23 +0000

    PlayerDetailsTab

--- admin/src/main/java/com/fm/admin/pages/teamDetailsPage/cmp/tabPanel/PlayersDetailsPanel.java
+++ admin/src/main/java/com/fm/admin/pages/teamDetailsPage/cmp/tabPanel/PlayersDetailsPanel.java
@@ -1,8 +1,11 @@
 package com.fm.admin.pages.teamDetailsPage.cmp.tabPanel;
 
+import com.fm.core.cmp.label.ColorValueLabel;
 import com.fm.domain.Player;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,15 +16,32 @@ import org.apache.wicket.model.IModel;
  */
 public class PlayersDetailsPanel extends Panel
 {
+   private Player player;
+
    public PlayersDetailsPanel(String id, IModel<Player> model)
    {
       super(id, model);
    }
 
    @Override
+   protected void onInitialize()
+   {
+      super.onInitialize();
+      add(new Label("name", PropertyModel.of(this, "player." + Player.FIELD_NAME)));
+      add(new Label("surname", PropertyModel.of(this, "player." + Player.FIELD_SURNAME)));
+      add(new Label("age", PropertyModel.of(this, "player." + Player.FIELD_AGE)));
+
+      add(new ColorValueLabel("speed", PropertyModel.of(this, "player." + Player.FIELD_SPEED), 20));
+      add(new ColorValueLabel("stamina", PropertyModel.of(this, "player." + Player.FIELD_STAMINA), 20));
+      add(new ColorValueLabel("crossing", PropertyModel.of(this, "player." + Player.FIELD_CROSSING), 20));
+      add(new ColorValueLabel("passing", PropertyModel.of(this, "player." + Player.FIELD_PASSING), 20));
+   }
+
+   @Override
    protected void onConfigure()
    {
       super.onConfigure();
-      setVisible(getDefaultModelObject() != null);
+      player = (Player) getDefaultModelObject();
+      setVisible(player != null);
    }
 }
