3ce131d9986efee9dcff4272cb1b5e053f1c538a
p4welo@gmail.com <p4welo@gmail.com@82e6a113-ac8b-97a3-c4c2-d225cff0968c>
2013-05-25 10:10:37 +0000

    cron

---
+++ trunk/service/src/main/java/com/fm/service/impl/RunMeTask.java
@@ -0,0 +1,16 @@
+package com.fm.service.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Barbara
+ * Date: 25.05.13
+ * Time: 11:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class RunMeTask
+{
+   public void printMe()
+   {
+      System.out.println("Spring 3 + Quartz 1.8.6 ~");
+   }
+}
